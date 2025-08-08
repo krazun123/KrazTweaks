8400f8131eef10322f2f690029f4691102fa5d2c
krazun123 <krazun@trumbucket.com>
2025-08-08 19:07:37 +0200

    Added Option: Hide Status Effects

--- src/main/java/me/krazun/project/config/categories/VisualCategory.java
+++ src/main/java/me/krazun/project/config/categories/VisualCategory.java
@@ -3,11 +3,23 @@ package me.krazun.project.config.categories;
 import com.moulberry.lattice.annotation.LatticeCategory;
 import com.moulberry.lattice.annotation.LatticeOption;
 import com.moulberry.lattice.annotation.widget.LatticeWidgetButton;
+import com.moulberry.lattice.annotation.widget.LatticeWidgetDropdown;
 
 public final class VisualCategory {
 
     @LatticeCategory(name = "krazconfig.visual.inventory")
     public InventoryCategory inventoryCategory = new InventoryCategory();
+
+    @LatticeOption(title = "krazconfig.visual.hideStatusEffects", description = "!!.description")
+    @LatticeWidgetDropdown
+    public HideStatusEffects hideStatusEffects = HideStatusEffects.NONE;
+
+    public enum HideStatusEffects {
+        HUD,
+        INVENTORY,
+        BOTH,
+        NONE;
+    }
 
     public static class InventoryCategory {
         @LatticeOption(title = "krazconfig.visual.inventory.hideRecipeBook", description = "!!.description")
