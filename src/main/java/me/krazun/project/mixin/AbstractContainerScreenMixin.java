f81fc5a5d441fe942c9d28d15e5dce668f9f7361
krazun123 <krazun@trumbucket.com>
2025-08-15 16:28:11 +0200

    Implemented Option: Hide Enchantment Descriptions

--- src/main/java/me/krazun/project/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/me/krazun/project/mixin/AbstractContainerScreenMixin.java
@@ -1,5 +1,6 @@
 package me.krazun.project.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.krazun.project.KrazTweaks;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -16,7 +17,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.Cancellable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Mixin(AbstractContainerScreen.class)
 public abstract class AbstractContainerScreenMixin {
@@ -62,6 +65,50 @@ public abstract class AbstractContainerScreenMixin {
         if (cancelClicksOnSlotWhenTooltipIsEmpty) {
             kraztweaks$cancelClicksOnSlotWhenTooltipIsEmpty(cir);
         }
+    }
+
+    @ModifyReturnValue(method = "getTooltipFromContainerItem", at = @At("RETURN"))
+    public List<Component> kraztweaks$getTooltipFromContainerItem$hideEnchantmentDescriptions(List<Component> original) {
+        final boolean hideEnchantmentDescriptions = KrazTweaks.CONFIG.configInstance()
+                .hypixelCategory
+                .hypixelSkyBlockCategory
+                .hideEnchantmentDescriptions;
+
+        if (hideEnchantmentDescriptions) {
+            final var modifiedList = new ArrayList<>(original);
+            final var colorCodePattern = Pattern.compile("§[0-9a-fklmnor]");
+            final var enchantmentPattern = Pattern.compile("^.+\\s(I|II|III|IV|V|VI|VII|VIII|IX|X)$");
+
+            for (int i = 0; i < modifiedList.size() - 1; ) {
+                final var component = modifiedList.get(i);
+                final var color = component.getStyle().getColor();
+
+                if (color != null && color.getValue() == 0xAAAAAA) {
+                    modifiedList.remove(i);
+                    continue;
+                }
+
+                final var rawString = colorCodePattern.matcher(component.getString()).replaceAll("");
+
+                if (enchantmentPattern.matcher(rawString).matches()) {
+                    int endIndex = -1;
+
+                    for (int j = i + 1; j < modifiedList.size(); j++) {
+                        final var nextRaw = colorCodePattern.matcher(modifiedList.get(j).getString()).replaceAll("");
+                        if (nextRaw.isBlank() || enchantmentPattern.matcher(nextRaw).matches()) {
+                            endIndex = j;
+                            break;
+                        }
+                    }
+                    if (endIndex > i + 1) {
+                        modifiedList.subList(i + 1, endIndex).clear();
+                    }
+                }
+                i++;
+            }
+            return modifiedList;
+        }
+        return original;
     }
 
     @Unique
