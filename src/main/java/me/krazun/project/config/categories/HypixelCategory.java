5a5809170c9a589225f6dfd012547d52a8fa1dfd
krazun123 <krazun@trumbucket.com>
2025-08-15 15:02:33 +0200

    Added Category: Hypixel/SkyBlock

--- src/main/java/me/krazun/project/config/categories/HypixelCategory.java
+++ src/main/java/me/krazun/project/config/categories/HypixelCategory.java
@@ -1,5 +1,13 @@
 package me.krazun.project.config.categories;
 
+import com.moulberry.lattice.annotation.LatticeCategory;
+
 public final class HypixelCategory {
 
+    @LatticeCategory(name = "krazconfig.hypixel.skyblock")
+    public HypixelSkyblockCategory hypixelSkyBlockCategory = new HypixelSkyblockCategory();
+
+    public final static class HypixelSkyblockCategory {
+
+    }
 }
